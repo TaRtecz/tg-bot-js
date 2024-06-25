d095c953a1f14d4a311d36768b1857fba93a4056
TaRtecz <35393884+TaRtecz@users.noreply.github.com>
2024-06-25 16:32:08 +0500

    Created bot

---
+++ index.js
@@ -0,0 +1,47 @@
+require("dotenv").config();
+const { Bot, GrammyError, HttpError } = require("grammy");
+// const options = require("./options.js");
+
+const bot = new Bot(process.env.BOT_API_KEY);
+
+const options = [
+  {
+    command: "start",
+    description: "Start Bot",
+  },
+  {
+    command: "info",
+    description: "Show info me",
+  },
+  {
+    command: "wow",
+    description: "Get me wow",
+  },
+];
+
+bot.api.setMyCommands(options);
+
+bot.command("start", async (ctx) => {
+  await ctx.reply("Hello! I am new bot by Bigdok86.");
+});
+
+bot.on("message", async (ctx) => {
+  console.log(ctx);
+  await ctx.reply("Your message - ", ctx.message.text);
+});
+
+bot.catch((error) => {
+  const ctx = error.ctx;
+  console.error(`Error while handling update ${ctx.update.update_id}`);
+  const e = error.error;
+
+  if (e instanceof GrammyError) {
+    console.error("Error in request:", e.description);
+  } else if (e instanceof HttpError) {
+    console.error("Could not contact Telegram:", e);
+  } else {
+    console.error("Unknown error:", e);
+  }
+});
+
+bot.start();
---
+++ options.js
@@ -0,0 +1,14 @@
+export const options = [
+  {
+    command: "start",
+    description: "Start Bot",
+  },
+  {
+    command: "info",
+    description: "Show info me",
+  },
+  {
+    command: "wow",
+    description: "Get me wow",
+  },
+];
